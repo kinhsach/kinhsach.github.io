4ddfa3571233696461d69ef494f9a13e51240ef4
Kinh Sách <itvtbadboy@gmail.com>
2025-08-21 04:33:51 +0700

    update static

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1755722099',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/tu-kinh/',
+      
+        '/the-sach/',
+      
+        '/luu-tru/',
+      
+        '/chep-kinh/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+

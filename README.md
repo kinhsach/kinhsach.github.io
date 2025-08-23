d0f99fe659031b0d6cef75a7b46f9bb287180a98
Kinh Sách <itvtbadboy@gmail.com>
2025-08-23 11:27:46 +0700

    update jekyll

--- README.md
+++
@@ -1,2 +0,0 @@
-# kinhsach.github.io
-Kinh sách miễn phí giúp cộng đồng đọc kinh nghe pháp tiếp thu trí tuệ giữ vững lòng tin sống đời Chánh Đạo
--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755722099',resources: [
+    cacheName: 'chirpy-1755923022',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

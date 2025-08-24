f935f830d8d99829eee59bd34894f63bf0a78fab
Kinh Sách <itvtbadboy@gmail.com>
2025-08-24 12:45:01 +0700

    update jekyll

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755948824',resources: [
+    cacheName: 'chirpy-1756014215',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
